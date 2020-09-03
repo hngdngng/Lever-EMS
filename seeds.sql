a7b457896526c48625419d0f31e5330b7431837b
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-09-03 00:55:58 -0400

    Updated initial role values

--- seeds.sql
+++ seeds.sql
@@ -2,7 +2,7 @@
 INSERT INTO department (name) VALUES ("Software Engineering");
 
 INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer", 140000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Junior developer", 140000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Junior developer", 80000, 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jill", "Bates", 1, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Perry", "Lage", 2, 1);
